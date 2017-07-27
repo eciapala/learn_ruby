23b3539df442a3fad1457b3f3bff62de0977ccdb
eciapala <elzbieta.ciapala@gmail.com>
2017-07-27 12:41:15 +0200

    add 02_calculator solution

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
-#write your code here
+def add(number1, number2)
+  number1 + number2
+end
+
+def subtract(number1, number2)
+  number1 - number2
+end
+
+def sum(numbers)
+  numbers.reduce(0, :+)
+end
+
+def multiply(numbers)
+  numbers.reduce(1, :*)
+end
+
+def power(number1, number2)
+  number1 ** number2
+end
+
+def factorial(number)
+  (1..number).reduce(1, :*)
+end
