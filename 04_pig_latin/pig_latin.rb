2cb3a35b94dad4312801ab6dc58ccbed6e8db018
eciapala <elzbieta.ciapala@gmail.com>
2017-07-30 07:46:07 +0200

    add 04_pig_latin solution

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,13 @@
-#write your code here
+def translate(input)
+  input.split.map do |word|
+    word =~ /^
+    (
+      [^aeiouyq]*
+      (qu)?
+    )
+    (.*)
+    first_part_of_word = $1
+    rest_of_word = $3
+    "#{rest_of_word}#{first_part_of_word}ay"
+  end.join(" ")
+end
