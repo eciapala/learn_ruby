01d8e303a3abd33c14916c2dbb232fbf76c41dec
eciapala <elzbieta.ciapala@gmail.com>
2017-08-04 13:34:38 +0200

    add new solution

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,10 @@
 class Book
-# write your code here
+  attr_reader :title
+
+  def title=(input)
+    small_words = ['the', 'and', 'a', 'an', 'in', 'of']
+    @title = input.capitalize.split.map do |word|
+      small_words.include?(word) ? word : word.capitalize
+    end.join(' ')
+  end
 end
